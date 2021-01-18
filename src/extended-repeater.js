94ade2652b76b2ac90df395c54d1224bb165ab81
Dmitriy <dmitriy.pavlov.17.04@gmail.com>
2021-01-18 20:57:58 +0300

    feat: complete 'extended-repeater' task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,41 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let string = '' + str;
+  let repeatString;
+  if (options.hasOwnProperty('addition')) {
+    let addition = '' + options.addition;
+    let arrayOfadditions = [];
+    if (!Number.isInteger(options.additionRepeatTimes)) {
+      arrayOfadditions.push(addition);
+    } else {
+      for (let i = 1; i <= options.additionRepeatTimes; i++) {
+        arrayOfadditions.push(addition);
+      }
+    }
+    let stringOfAddition;
+    if (!options.hasOwnProperty('additionSeparator')) {
+      stringOfAddition = arrayOfadditions.join('|');
+    } else {
+      stringOfAddition = arrayOfadditions.join(options.additionSeparator);
+    }
+    repeatString = string + stringOfAddition;
+  } else {
+    repeatString = string;
+  }
+  let arrayOfrepeatString = [];
+  if (!Number.isInteger(options.repeatTimes)) {
+    arrayOfrepeatString.push(repeatString);
+  } else {
+    for (let i = 1; i <= options.repeatTimes; i++) {
+      arrayOfrepeatString.push(repeatString);
+    }
+  }
+  let result;
+  if (!options.hasOwnProperty('separator')) {
+    result = arrayOfrepeatString.join('+');
+  } else {
+    result = arrayOfrepeatString.join(options.separator);
+  }
+  return result;
 };
-  
